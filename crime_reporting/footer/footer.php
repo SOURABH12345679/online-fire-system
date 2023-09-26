6d16bab9708bac1369b6c9ca878e3e7eff1d0ca0
SOURABH12345679 <141850073+SOURABH12345679@users.noreply.github.com>
2023-09-26 14:38:40 +0530

    Add files via upload

---
+++ crime_reporting/footer/footer.php
@@ -0,0 +1,5 @@
+<!-- Start Footer -->
+<footer class="container-fluid bg-dark text-center p-2">
+<small class="text-white"> Copyright &copy;2023 || Desigend By Sourabh Kumar ||<a href="officelogin.php" data-toggle="modal" data-target="#AdminLogModalCenter"> Admin login </a> </small>
+</footer>
+<!-- End Footer -->
